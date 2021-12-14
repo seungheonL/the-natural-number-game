f9b98ac5d462146cf7a667d7054ada74be6946a5
seungheonL <44883967+seungheonL@users.noreply.github.com>
2021-12-14 22:39:34 +0900

    Create level1.lean

---
+++ 1-addition-world/level1.lean
@@ -0,0 +1,12 @@
+lemma add_right_comm (a b c : mynat) : a + b + c = a + c + b :=
+begin
+  induction c with d hd,
+  rw add_zero,
+  rw add_zero,
+  refl,
+  rw add_succ,
+  rw add_succ,
+  rw succ_add,
+  rw hd,
+  refl,
+end

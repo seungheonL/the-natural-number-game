e17aea49b35e10555c0077071d005bfa04a5fa74
seungheonL <44883967+seungheonL@users.noreply.github.com>
2021-12-15 22:13:55 +0900

    Create level9.lean

---
+++ 3-proposition-world/level9.lean
@@ -0,0 +1,16 @@
+example (A B C D E F G H I J K L : Prop)
+(f1 : A → B) (f2 : B → E) (f3 : E → D) (f4 : D → A) (f5 : E → F)
+(f6 : F → C) (f7 : B → C) (f8 : F → G) (f9 : G → J) (f10 : I → J)
+(f11 : J → I) (f12 : I → H) (f13 : E → H) (f14 : H → K) (f15 : I → L)
+ : A → L :=
+begin
+  intro a,
+  have b := f1(a),
+  have e := f2(b),
+  have f := f5(e),
+  have g := f8(f),
+  have j := f9(g),
+  have i := f11(j),
+  have l := f15(i),
+  exact l,  
+end
